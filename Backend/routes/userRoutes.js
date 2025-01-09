64171709acde114c1cdf10ef2086a771117bb23e
Aweary69 <161394447+Aweary69@users.noreply.github.com>
2025-01-09 17:21:59 +0500

    Add files via upload

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require("../controllers/usersController");
-const validateUser = require("../middleware/validateUser");
+
+
+
+const { registerUser, loginUser, getUser } = require('../controllers/userControllers.js');
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.post("/", validateUser, createUser);
-router.put("/:id", validateUser, updateUser);
-router.delete("/:id", deleteUser);
+
+
+router.post('/register', registerUser);
+
+// Route to login a user
+router.post('/login', loginUser);
+
+
+
+
+
+// router.get("/", getUsers);
+// router.get("/:id", getUserById);
+// router.post("/", validateUser, createUser);
+// router.put("/:id", validateUser, updateUser);
+// router.delete("/:id", deleteUser);
 
 module.exports = router;
